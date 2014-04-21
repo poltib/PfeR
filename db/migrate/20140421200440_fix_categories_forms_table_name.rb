1504b2f9b049587d0ce6da7f26c30eca9f83ee87
Plop <thiry.jeremy@gmail.com>
2014-04-21 22:30:03 +0200

    Rename migration because I already run it in production when it was empty

--- db/migrate/20140421200440_fix_categories_forms_table_name.rb
+++ db/migrate/20140421200440_fix_categories_forms_table_name.rb
@@ -1,4 +1,4 @@
-class FixCategoriesForumsTableName < ActiveRecord::Migration
+class FixCategoriesFormsTableName < ActiveRecord::Migration
   def change
     rename_table :categories_forms, :categories_forums
   end
