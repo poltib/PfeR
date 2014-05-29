23a6fcf7f106a312a1df78aaf16d1da1f9f190c3
Plop <thiry.jeremy@gmail.com>
2014-05-29 09:39:03 +0200

    [edit] Facebook omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,8 +45,8 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.avatar = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name
+      user.avatar = auth.info.image
     end
   end
 
