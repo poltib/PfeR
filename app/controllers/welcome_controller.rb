fc3c57beb3b7c7ac2746f904909c96b5284f094e
Plop <thiry.jeremy@gmail.com>
2014-05-30 16:43:20 +0200

    [edit] front-page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@happenings = Happening.search(params[:event_type], params[:date], params[:location])
+  	@happenings = Happening.last(5)
+  	@tracks = Track.last(5)
   	@user = User.new
   end
 end
