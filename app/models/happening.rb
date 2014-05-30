69aba51264016d87cfafc0b4462b45aab2ede202
Plop <thiry.jeremy@gmail.com>
2014-05-30 17:12:11 +0200

    Links aren't mandatory

--- app/models/happening.rb
+++ app/models/happening.rb
@@ -4,7 +4,7 @@ class Happening < ActiveRecord::Base
   geocoded_by :address  
   after_validation :geocode, :if => :address_changed? 
 
-  validates :name, :description, :address, :link, :date, presence: true
+  validates :name, :description, :address, :date, presence: true
 
   has_many :tracks
   
