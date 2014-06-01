fbcd71bf17e6ad85b79880218dde4e4f2233d368
Plop <thiry.jeremy@gmail.com>
2014-06-01 08:48:27 +0200

    remove paperclip from sponsor

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
-	mount_uploader :image, ImageUploader, :delayed=> true 
-	
+	mount_uploader :image, ImageUploader, :delayed=> true
+
 	belongs_to :imagable, :polymorphic => true
 	belongs_to :user
 end
