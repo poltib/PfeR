43617ada642e5ae0c17cfc09dd5fbfa040cb3899
Plop <thiry.jeremy@gmail.com>
2014-06-01 23:05:59 +0200

    Some styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
   	@happenings = Happening.last(5)
   	@tracks = Track.last(5)
+  	@groups = Group.last(5)
+  	@forums = Forum.last(5)
   	@user = User.new
   end
 end
