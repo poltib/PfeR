414611115f34dc6bb992be0034cdd967fa661037
Plop <thiry.jeremy@gmail.com>
2014-06-04 17:26:01 +0200

    forms style and fix some html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
